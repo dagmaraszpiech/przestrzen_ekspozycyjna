44ef4b2d2088bf986c65d6910b218ab43fda7889
dagmaraszpiech <dagmaraszpiech@gmail.com>
2025-02-03 03:46:21 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,435 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-otoczenie",
+      "name": "otoczenie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-ekspozycja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ekspozycja",
+      "name": "ekspozycja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wejscie",
+      "name": "wejscie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04233689141622676,
+          "pitch": -0.040464035061681614,
+          "rotation": 0,
+          "target": "3-bliskie-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bliskie-wejscie",
+      "name": "bliskie wejscie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.061049382821828146,
+          "pitch": -0.04369884125322088,
+          "rotation": 0,
+          "target": "4-wnetrze"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-wnetrze",
+      "name": "wnetrze",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.537644501334162,
+          "pitch": 0.10193641282055665,
+          "rotation": 0,
+          "target": "10-czerwone"
+        },
+        {
+          "yaw": 0.680025241105966,
+          "pitch": 0.026389582601202832,
+          "rotation": 0,
+          "target": "5-box-szklo-kopula"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-box-szklo-kopula",
+      "name": "box szklo kopula",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22661802495731465,
+          "pitch": -0.03267347589344993,
+          "rotation": 0,
+          "target": "6-kopula-na-podescie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kopula-na-podescie",
+      "name": "kopula na podescie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1881435968873184,
+          "pitch": 0.04727773294040105,
+          "rotation": 0,
+          "target": "7-wejscie-kamien"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-wejscie-kamien",
+      "name": "wejscie kamien",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7246861716098163,
+          "pitch": 0.13236848655752098,
+          "rotation": 0,
+          "target": "8-kamien-wnetrze"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-kamien-wnetrze",
+      "name": "kamien wnetrze",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8583677536142886,
+          "pitch": 0.1053468137462481,
+          "rotation": 0,
+          "target": "9-wejscie-biel"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-wejscie-biel",
+      "name": "wejscie biel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31626559873084226,
+          "pitch": 0.006731114959812601,
+          "rotation": 0,
+          "target": "10-czerwone"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-czerwone",
+      "name": "czerwone",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2056992032276632,
+          "pitch": 0.19444754540969456,
+          "rotation": 0,
+          "target": "2-wejscie"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
